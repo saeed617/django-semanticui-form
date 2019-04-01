05d39ac23f19fec6429b16f6c37a939b09f04165
saeed <stahmasebi30@gmail.com>
2019-04-01 19:44:31 +0430

    add SelectMultiple + SelectSearchMultiple widgets

--- semanticuiform/widgets.py
+++ semanticuiform/widgets.py
@@ -13,3 +13,17 @@ class SemanticSearchSelect(widgets.Select):
         super(SemanticSearchSelect, self).__init__(attrs=attrs, choices=choices)
         if 'class' not in self.attrs:
             self.attrs['class'] = 'ui search dropdown semantic-dropdown'
+
+
+class SemanticSelectMultiple(widgets.SelectMultiple):
+    def __init__(self, attrs=None, choices=()):
+        super(SemanticSelectMultiple, self).__init__(attrs, choices)
+        if 'class' not in self.attrs:
+            self.attrs['class'] = 'ui dropdown semantic-dropdown'
+
+
+class SemanticSearchSelectMultiple(widgets.SelectMultiple):
+    def __init__(self, attrs=None, choices=()):
+        super(SemanticSearchSelectMultiple, self).__init__(attrs, choices)
+        if 'class' not in self.attrs:
+            self.attrs['class'] = 'ui search dropdown semantic-dropdown'
