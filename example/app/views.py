e4c31f7f648fcdfa4cd42864215957f8319cde5d
saeed <stahmasebi30@gmail.com>
2019-04-01 15:07:07 +0430

    add new example app

--- example/app/views.py
+++ example/app/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from app.forms import ExampleForm
 
-def index(request):
-    form = ExampleForm()
-
-    return render(request, 'index.html', {'form': form})
+# Create your views here.
