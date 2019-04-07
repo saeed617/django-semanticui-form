3393e703e9df2a0d8ccf44cf6b0106c25c294dc5
saeed <stahmasebi30@gmail.com>
2019-04-07 19:36:44 +0430

    bug fix

--- semanticuiform/__init__.py
+++ semanticuiform/__init__.py
@@ -1,5 +1,1 @@
-from .meta import VERSION
-
-
-__version__ = str(VERSION)
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-semanticui-form',
-    version='1.0.0',
+    version='1.0.1',
     description="django-semanticui-form",
     classifiers=[
         "Programming Language :: Python",
